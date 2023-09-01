73be55409b8b75e33b1ea795b03f1ccb4d963c8e
Santiago Morera <santi.iztli@gmail.com>
2023-08-31 22:32:24 -0600

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,6 @@ const IndexPage = async () => {
       <section id="index">
         <div className="mainPage-container animate__animated animate__fadeIn">
           <h1 className="color_title rainbow_bg">Mi BLOG</h1>
-          <p className="title_description">
-            Hola, soy ingeniero de software senior en la empresa. Disfruto
-            trabajando con Next.js y creando hermosas experiencias de front-end.
-          </p>
           <p>Elige el Post de tu preferencia</p>
           <Blog />
         </div>
